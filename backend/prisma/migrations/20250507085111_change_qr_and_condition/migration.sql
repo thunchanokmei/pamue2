96946d266dc704d29fb4f965a15df63eee422855
thunchanokmei <89442821+thunchanokmei@users.noreply.github.com>
2025-05-07 15:56:27 +0700

    up

---
+++ backend/prisma/migrations/20250507085111_change_qr_and_condition/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `condition` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "condition",
+ADD COLUMN     "condition" INTEGER;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "QRurl" TEXT;
